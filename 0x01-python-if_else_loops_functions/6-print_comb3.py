ab7be7463914e5c597b52448b6d440b06107e9a0
Krabs <krabo24@yahoo.com>
2023-08-15 10:02:29 -0700

    update1

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for i in range(10):
+for i in range(1, 9):
     for j in range(i + 1, 10):
-        if i * 10 + j <= 89:
-            print("{:02d}".format(i * 10 + j), end=", ")
+        print("{:02d}".format(i * 10 + j), end=", ")
 print("89")
