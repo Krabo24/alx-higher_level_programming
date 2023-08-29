e8b5b0f3edf453a4f1ab7ee47d868e56117c7603
Krabs <krabo24@yahoo.com>
2023-08-29 14:59:00 -0700

    Write a class Square that defines a square by: (based on 4-square.py)

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+"""define class Square"""
+
+class Square:
+    """represent Square"""
+
+    def __init__(self, size=0):
+        """initialize new Square
+
+        Args:
+            size (int): size of new Square
+        """
+
+        self.size = size
+
+    @property
+    def size(self):
+        """get or set current size of Square"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """return current area of Square"""
+        return (self.__size * self.__size)
+
+    def __eq__(self, other):
+        """define the == comparision to Square"""
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        """define the != comparison to Square"""
+        return self.area() != other.area()
+
+    def __lt__(self, other):
+        """define the < comparison to a Square"""
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        """define the <= comparison to a Square"""
+        return self.area() <= other.area()
+
+    def __gt__(self, other):
+        """define the > comparison to a Square"""
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        """define the >= compmarison to a Square"""
+        return self.area() >= other.area()
